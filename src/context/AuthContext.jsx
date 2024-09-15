5073f9ff00c187a8f282f37b1d0f652347c1f6e5
Romy <romycardozopy@gmail.com>
2024-09-15 15:58:07 -0400

    setIsAuthenticated(true) modificado a false para usar el login

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -23,7 +23,7 @@ export const AuthProvider = ({ children }) => {
 					const currentUser = await getCurrentUser(token);
 					console.log({currentUser})
 					setUser(currentUser);
-					setIsAuthenticated(true);//cambiar cuando tenga mi logica de token
+					setIsAuthenticated(false);//cambiar cuando tenga mi logica de token
 				} catch (err) {
 					console.error(
 						"Failed to fetch user during initialization",
