c99783bcf3f2cdb2d4ff88c3ae060771ace8685d
RomyCardozo <romycardozopy@gmail.com>
2024-10-12 19:10:49 -0300

    ocultamos modulo "Usuarios " para rol usuario

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { MenuItem } from "./MenuItem";
+import { useAuth } from '../context/AuthContext';
 import {
 	FaUsers,
 	FaServicestack,
@@ -11,7 +12,7 @@ import {
 import { GiHairStrands } from "react-icons/gi";
 import { useLocation } from "react-router-dom";
 
-const menuItems = [
+/*const menuItems = [
 	{ icon: <FaUsers />, title: "Clientes", link: "/home/clientes" },
 	{ icon: <GiHairStrands />, title: "Servicios", link: "/home/servicios" },
 	{ icon: <FaUser />, title: "Usuarios", link: "/home/usuarios" },
@@ -19,10 +20,20 @@ const menuItems = [
 	{ icon: <FaChartBar />, title: "Informes", link: "/home/informes" },
 	{ icon: <FaCog />, title: "Configuración", link: "/home/configuracion" },
 	//{ icon: <FaUserCircle />, title: "Perfil", link: "/home/perfil" },
-];
+];*/
 export const Sidebar = () => {
   const location = useLocation();
+  const { user } = useAuth(); // Accede al contexto
 
+  // Ejemplo de uso del rol del usuario para condicionar la visualización
+  const menuItems = [
+	  { icon: <FaUsers />, title: 'Clientes', link: '/home/clientes' },
+	  { icon: <GiHairStrands />, title: 'Servicios', link: '/home/servicios' },
+	  user?.rol?.descripcion === 'Administrador' ? { icon: <FaUser />, title: 'Usuarios', link: '/home/usuarios' } : null,
+	  { icon: <FaShoppingCart />, title: 'Venta', link: '/home/venta' },
+	  { icon: <FaChartBar />, title: 'Informes', link: '/home/informes' },
+	  { icon: <FaCog />, title: 'Configuración', link: '/home/configuracion' },
+  ].filter(Boolean); 
 
 	return (
 <div className="w-64 text-white flex-shrink-0 mt-16 p-4 bg-gradient-to-r from-purple-500 via-purple-400 to-purple-500 shadow-lg rounded-lg">
